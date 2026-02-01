94729ba597fa80e214cd95d461804c34eedafd00
luniakinihor-arch <luniakinihor@gmail.com>
2026-02-01 12:56:00 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import Fastify from "fastify";
 import dotenv from "dotenv";
 import analyzeRoute from "./routes/analyze.route";
-import {AnalyzePipeline} from "./pipline/analyze-pipline";
 
 dotenv.config();
 
 const fastify = Fastify({ logger: true });
 
-AnalyzePipeline.init()
 fastify.register(analyzeRoute, { prefix: '/api' });
 const PORT = Number(process.env.PORT) || 3100;
 fastify.listen({ port: PORT, host: "0.0.0.0" }).then(() => {
